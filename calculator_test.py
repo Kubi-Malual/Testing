b58892e130685a2d5716b01c56156997a25288fc
Kubi-Malual <kubimalual25@gmail.com>
2022-03-02 17:52:40 +0300

    added the extra 2 files

--- calculator_test.py
+++ calculator_test.py
@@ -14,9 +14,9 @@ class Test(unittest.TestCase):
     def test_0_mult(self):
         result = self.calculator.multiply(4,5)
         self.assertEqual(result,20)
-    # def test_0_power(self):
-    #     result = self.calculator.power(4,2)
-    #     self.assertEqual(result,16)
+    def test_0_power(self):
+        result = self.calculator.power(4,2)
+        self.assertEqual(result,16)
 if __name__ == '__main__':
     # begin the unittest.main()
     unittest.main()

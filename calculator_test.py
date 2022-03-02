e7743cac666d0bdf353c7d9e9f96991163ec233e
Kubi-Malual <kubimalual25@gmail.com>
2022-03-02 18:41:12 +0300

    all the 5 mathmatical opertions

--- calculator_test.py
+++ calculator_test.py
@@ -11,15 +11,15 @@ class Test(unittest.TestCase):
     def test_1_subtract(self):
         result = self.calculator.subtract(4,8)
         self.assertEqual(result,-4)
-    # def test_0_mult(self):
-    #     result = self.calculator.multiply(4,5)
-    #     self.assertEqual(result,20)
-    # def test_0_divide(self):
-    #     result = self.calculator.divide(4,2)
-    #     self.assertEqual(result,2)
-    # def test_0_power(self):
-    #     result = self.calculator.power(4,2)
-    #     self.assertEqual(result,16)
+    def test_0_mult(self):
+        result = self.calculator.multiply(4,5)
+        self.assertEqual(result,20)
+    def test_0_divide(self):
+        result = self.calculator.divide(4,2)
+        self.assertEqual(result,2)
+    def test_0_power(self):
+        result = self.calculator.power(4,2)
+        self.assertEqual(result,16)
 if __name__ == '__main__':
     # begin the unittest.main()
     unittest.main()

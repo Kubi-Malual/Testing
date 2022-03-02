f306e3c48ec7eb80200c726521f6deabbb8072c6
Kubi-Malual <kubimalual25@gmail.com>
2022-03-02 18:30:25 +0300

    Doing on only 1

--- calculator_test.py
+++ calculator_test.py
@@ -7,19 +7,19 @@ class Test(unittest.TestCase):
     calculator = CalculatorClass.Calculator() # instantiate the Calculator Class
     def test_0_add(self):
         result = self.calculator.add(4,8)
-        self.assertEqual(result,12)
-    def test_0_subtract(self):
-        result = self.calculator.subtract(4,8)
-        self.assertEqual(result,-4)
-    def test_0_mult(self):
-        result = self.calculator.multiply(4,5)
-        self.assertEqual(result,20)
-    def test_0_divide(self):
-        result = self.calculator.divide(4,2)
         self.assertEqual(result,2)
-    def test_0_power(self):
-        result = self.calculator.power(4,2)
-        self.assertEqual(result,16)
+    # def test_0_subtract(self):
+    #     result = self.calculator.subtract(4,8)
+    #     self.assertEqual(result,-4)
+    # def test_0_mult(self):
+    #     result = self.calculator.multiply(4,5)
+    #     self.assertEqual(result,20)
+    # def test_0_divide(self):
+    #     result = self.calculator.divide(4,2)
+    #     self.assertEqual(result,2)
+    # def test_0_power(self):
+    #     result = self.calculator.power(4,2)
+    #     self.assertEqual(result,16)
 if __name__ == '__main__':
     # begin the unittest.main()
     unittest.main()
